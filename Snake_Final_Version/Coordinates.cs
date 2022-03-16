95edfc4bcb38af7c6c593013cf8e5afe98287274
Bozhan Bozhanov <bojanbojanovsot@gmail.com>
2022-03-16 12:49:09 +0200

    Add_Snake_Update_With_Classes

---
+++ Snake_Final_Version/Coordinates.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class Coordinates
+    {
+        public int Row; 
+        public int Col;
+        public Coordinates(int row, int col)
+        {
+            this.Row = row;
+            this.Col = col;
+        }
+    }
+}
---
+++ Snake_Final_Version/Visualizer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class Visualizer
+    {
+        public  void Write(string text, int row, int col, ConsoleColor color = ConsoleColor.Black)
+        {
+            Console.SetCursorPosition(col, row);
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ResetColor();
+        }
+    }
+}
