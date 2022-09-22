56aa77b6ba5915dce3da37a097431aea1313dada
DareBDeviL <bojanbojanovsot@gmail.com>
2022-09-22 17:06:27 +0300

    same

---
+++ Snake/UserDatabase/Account.cs
@@ -0,0 +1,63 @@
+﻿namespace UserDatabase
+{
+    using Interfaces;
+
+    public class Account : IAccount
+    {
+        private string username;
+        private string password;
+        private int score;
+
+        public Account(string username, string password, int score)
+        {
+            this.Username = username;
+            this.Password = password;
+            this.Score = score;
+        }
+
+
+        public string Username
+        {
+            get => this.username;
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Username can not be empty!");
+                }
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Username must be at least 3 characters long!");
+                }
+                this.username = value;
+            }
+        }
+
+        public string Password
+        {
+            get => this.password;
+            private set
+            {
+                if (!String.IsNullOrWhiteSpace(value) && value.Length < 4)
+                {
+                    throw new ArgumentException("Password must be at least 4 symbols long!");
+                }
+                this.password = value!;
+            }
+        }
+
+        public int Score
+        {
+            get => this.score;
+            set => this.score = value;
+        }
+
+        public bool IsBlocked { get; set; } = false;
+
+        public DateTime LastBlockedTime { get; set; }
+
+        public DateTime LastLoggedInTime { get; set; }
+
+        public DateTime AccountCreatedTime { get; set; }
+    }
+}
---
+++ Snake/UserDatabase/Interfaces/IAccount.cs
@@ -0,0 +1,19 @@
+﻿namespace UserDatabase.Interfaces
+{
+    public interface IAccount
+    {
+        public string Username { get;}
+
+        public string Password { get;}
+
+        public int Score { get; set; }
+
+        public bool IsBlocked { get; set; }
+
+        public DateTime LastBlockedTime { get; set; }
+
+        public DateTime LastLoggedInTime { get; set; }
+
+        public DateTime AccountCreatedTime { get; set; }
+    }
+}
